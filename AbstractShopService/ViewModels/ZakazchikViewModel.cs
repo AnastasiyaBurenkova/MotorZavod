aaf9cfbcba762b5e1dee19d188ca6d6167194022
AnastasiyaBurenkova <31570128+AnastasiyaBurenkova@users.noreply.github.com>
2018-05-10 23:00:33 +0400

    Add files via upload

--- AbstractShopService/ViewModels/ZakazchikViewModel.cs
+++ AbstractShopService/ViewModels/ZakazchikViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System.Runtime.Serialization;
+﻿using System.Collections.Generic;
+using System.Runtime.Serialization;
 
 namespace AbstractShopService.ViewModels
 {
@@ -8,6 +9,10 @@ namespace AbstractShopService.ViewModels
         [DataMember]
         public int Id { get; set; }
         [DataMember]
+        public string Mail { get; set; }
+        [DataMember]
         public string ZakazchikFIO { get; set; }
+        [DataMember]
+        public List<MessageInfoViewModel> Messages { get; set; }
     }
 }
