49212110bdd66818ceaf579f071d72fe93007f72
AnastasiyaBurenkova <31570128+AnastasiyaBurenkova@users.noreply.github.com>
2018-05-10 22:16:17 +0400

    Add files via upload

---
+++ AbstractShopModel/MessageInfo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace AbstractShopModel
+{
+    public class MessageInfo
+    {
+        public int Id { get; set; }
+
+        public string MessageId { get; set; }
+
+        public string FromMailAddress { get; set; }
+
+        public string Subject { get; set; }
+
+        public string Body { get; set; }
+
+        public DateTime DateDelivery { get; set; }
+
+        public int? ZakazchikId { get; set; }
+
+        public virtual Zakazchik Zakazchik { get; set; }
+    }
+}
