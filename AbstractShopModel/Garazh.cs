d550ef2acac03eb7876d66428dabee1830bd9d2d
AnastasiyaBurenkova <31570128+AnastasiyaBurenkova@users.noreply.github.com>
2018-05-10 22:03:02 +0400

    Add files via upload

--- AbstractShopModel/Garazh.cs
+++ AbstractShopModel/Garazh.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace AbstractShopModel
 {
@@ -13,6 +16,9 @@ namespace AbstractShopModel
     {
         public int Id { get; set; }
 
+        [Required]
         public string GarazhName { get; set; }
+        [ForeignKey("GarazhId")]
+        public virtual List<GarazhDetali> GarazhDetalis { get; set; }
     }
 }
