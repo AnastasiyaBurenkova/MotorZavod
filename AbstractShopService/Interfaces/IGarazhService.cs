cd2844a1440644ad76775ff93837da08f61ee2c1
AnastasiyaBurenkova <31570128+AnastasiyaBurenkova@users.noreply.github.com>
2018-05-20 12:14:01 +0400

    Add files via upload

--- AbstractShopService/Interfaces/IGarazhService.cs
+++ AbstractShopService/Interfaces/IGarazhService.cs
@@ -1,19 +1,22 @@
-﻿using AbstractShopService.BindingModels;
+﻿using AbstractShopService.Attributies;
+using AbstractShopService.BindingModels;
 using AbstractShopService.ViewModels;
 using System.Collections.Generic;
 
 namespace AbstractShopService.Interfaces
 {
+    [CustomInterface("Интерфейс для работы со складами")]
     public interface IGarazhService
     {
+        [CustomMethod("Метод получения списка складов")]
         List<GarazhViewModel> GetList();
-
+        [CustomMethod("Метод получения склада по id")]
         GarazhViewModel GetElement(int id);
-
+        [CustomMethod("Метод добавления склада")]
         void AddElement(GarazhBindingModel model);
-
+        [CustomMethod("Метод изменения данных по складу")]
         void UpdElement(GarazhBindingModel model);
-
+        [CustomMethod("Метод удаления склада")]
         void DelElement(int id);
     }
 }
